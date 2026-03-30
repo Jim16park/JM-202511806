5b12b80052d9d0f10c686e548fabe37b875cd4e4
jim park <13parkjimena16@gmail.com>
2026-03-30 18:30:19 -0500

    Ejemplo de Modelado de Clase y generar codigo a partir del modelo

---
+++ VPM/pe/edu/upeu/Persona.java
@@ -0,0 +1,61 @@
+package pe.edu.upeu;
+
+public class Persona {
+
+	private String nombre = "Pedro";
+	protected int edad;
+	private int talla;
+	private String dni;
+
+	public void caminar() {
+		// TODO - implement Persona.caminar
+		throw new UnsupportedOperationException();
+	}
+
+	public void dormir() {
+		// TODO - implement Persona.dormir
+		throw new UnsupportedOperationException();
+	}
+
+	public void getTalla() {
+		// TODO - implement Persona.getTalla
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param talla
+	 */
+	public void setTalla(int talla) {
+		this.talla = talla;
+	}
+
+	public void getDni() {
+		// TODO - implement Persona.getDni
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param dni
+	 */
+	public void setDni(String dni) {
+		// TODO - implement Persona.setDni
+		throw new UnsupportedOperationException();
+	}
+
+	public Persona() {
+		// TODO - implement Persona.Persona
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param dni
+	 */
+	public Persona(String dni) {
+		// TODO - implement Persona.Persona
+		throw new UnsupportedOperationException();
+	}
+
+}
